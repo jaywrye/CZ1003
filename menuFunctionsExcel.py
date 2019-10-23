222b0cb6d1bc52cff853a8d4beb844e51ba0254f
ongyishen24 <55795007+ongyishen24@users.noreply.github.com>
2019-10-23 15:41:38 +0800

    Update menuFunctionsExcel.py

--- menuFunctionsExcel.py
+++ menuFunctionsExcel.py
@@ -70,7 +70,7 @@ def storeopeningtime(storename, day, storeOpenDict):                            
 def storemenu(storename, menuDict):                                            # Function that returns dictionary of ALL items on their menu {"Name of food item" : price of food}
     storemenuDict = {}                                                         # storename is a string
     for n in menuDict:
-        if storename in n:
+        if lower(storename) == lower(n[0]):
             storemenuDict.update({n[1] : menuDict[n]})
     return storemenuDict
 
